06e5012e9d54837d89c39c0ae59f31c8069b348e
LaianeLiboni <69061205+LaianeLiboni@users.noreply.github.com>
2021-02-09 00:14:06 -0300

    Add files via upload
    Exercícios da terceira Semana do Campinas Tech - Programa que controla uma conta bancária.

---
+++ Semana3/Cliente.java
@@ -0,0 +1,17 @@
+package Semana3;
+
+public class Cliente {
+	
+	public Cliente(String nome) {
+		this.nome = nome;
+	}
+	
+	private String nome;
+
+	public String getNome() {
+		return nome;
+	}
+	
+	
+}
+
---
+++ Semana3/ContaPoupanca.java
@@ -0,0 +1,8 @@
+package Semana3;
+
+public class ContaPoupanca extends Conta {
+
+	public ContaPoupanca(Double saldo, Cliente cliente) {
+		super(saldo, cliente);
+	}
+}
